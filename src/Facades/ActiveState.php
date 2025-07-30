1de86e9f2c46bbfc403b3d0195ac3ec80f72ecda
Julian Schramm <julian.schramm@farbcode.net>
2025-07-30 14:15:20 +0200

    chore: update facade PHPDoc

--- src/Facades/ActiveState.php
+++ src/Facades/ActiveState.php
@@ -3,13 +3,19 @@
 namespace Farbcode\StatefulResources\Facades;
 
 use Farbcode\StatefulResources\ActiveState as ActiveStateService;
+use Farbcode\StatefulResources\Contracts\ResourceState;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static void setShared(string $state)
+ * @method static void setShared(string|ResourceState $state)
  * @method static string getShared()
- * @method static void setForResource(string $resourceClass, string $state)
+ * @method static void setForResource(string $resourceClass, string|ResourceState $state)
  * @method static string getForResource(string $resourceClass)
+ * @method static bool matchesShared(string|ResourceState $state)
+ * @method static bool matchesResource(string $resourceClass, string|ResourceState $state)
+ * @method static string get(?string $resourceClass = null)
+ * @method static void set(string|ResourceState $state, ?string $resourceClass = null)
+ * @method static bool matches(string|ResourceState $state, ?string $resourceClass = null)
  */
 class ActiveState extends Facade
 {
